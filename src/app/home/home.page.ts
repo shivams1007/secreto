f2c7de9796f229e1b383de204350cec76cd6f204
Shivam Singh <sshivam478@gmail.com>
2024-05-19 23:10:26 +0530

    signin condition added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,24 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonIcon, IonLabel, IonContent, IonButton, IonPopover, IonItem, IonList, IonInput, IonInputPasswordToggle } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { keypad } from 'ionicons/icons';
+import { keypad, fingerPrint } from 'ionicons/icons';
 import { InputComponent } from '../shared/component/input/input.component';
 import { ButtonComponent } from '../shared/component/button/button.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonInput, IonItem, IonPopover, IonButton, IonContent, IonIcon, IonLabel, IonList, InputComponent, ButtonComponent],
+  imports: [IonInput, IonItem, IonPopover, IonButton, IonContent, IonIcon, IonLabel, IonList, InputComponent, ButtonComponent, NgIf],
 })
 export class HomePage {
   public environmentInjector = inject(EnvironmentInjector);
   keypadType: string = 'text';
+  isCreate: boolean = false;
   constructor() {
-    addIcons({ keypad });
+    addIcons({ keypad, fingerPrint });
   }
 
   changeKeypadType(type: string) {
